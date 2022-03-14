83b3413cad37bbd2ca15123cce9481d241013975
GTourinho <gabrieltourinho@hotmail.com>
2022-03-14 15:23:48 -0300

    Teste com árvore

--- src/background.js
+++ src/background.js
@@ -1,9 +1,14 @@
+// Imports
+var TreeModel = require('tree-model');
+
+// Google API
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = "https://apis.google.com/js/client.js";
 head.appendChild(script);
 
+// Espera carregamento GAPI
 window.onload = function () {
   setTimeout(function () {
     gapi.load('client:auth2', onGAPILoad);
@@ -13,6 +18,8 @@ window.onload = function () {
 const API_KEY = 'AIzaSyD0S0AnY3gPu5xkeMWYbZpftsh_BOndE5s';
 const DISCOVERY_DOCS = ["https://classroom.googleapis.com/$discovery/rest?version=v1"];
 
+
+// Inicia a API do Classroom (Depois de carregar o GAPI)
 function onGAPILoad() {
   gapi.client.init({
     apiKey: API_KEY,
@@ -31,6 +38,7 @@ function gapi_loaded() {
       'access_token': token,
     });
 
+    // Manipulação da API
     gapi.client.classroom.courses.courseWork.list({courseId: '412519657549'}).then(function(response) {
 
       var courseWorks = response.result.courseWork;
@@ -38,6 +46,18 @@ function gapi_loaded() {
       for(var i = 0; i < courseWorks.length; i++){
         console.log(courseWorks[i])
       }
+
+    tree = new TreeModel(),
+    root = tree.parse({name: 'a', children: [{name: 'b'}]});
+
+    var b = root.first(function (node) {
+    return node.model.name === 'b';});
+    
+    b.addChild(tree.parse({name: 'c'}))
+
+    root.walk({strategy: 'post'}, function (node) {
+      console.log(node.model.name)
+    });
 
     });
   })
