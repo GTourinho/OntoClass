05e1dbbe4bfdad9c44acafa70110c385f665cb8b
GTourinho <gabrieltourinho@hotmail.com>
2022-05-10 22:52:03 -0300

    Resgate da competencia na ontologia + Remoçao do grafico de arvore

--- dist/competencies.js
+++ dist/competencies.js
@@ -44785,12 +44785,13 @@ const N3 = __webpack_require__(/*! n3 */ "./node_modules/n3/src/index.js");
 
 // Variaveis
 var tree = new TreeModel();
-var competencias = tree.parse({name: 'competencias', level: 'black'});
+var competencias = tree.parse({name: 'competencias'});
 var ontology;
+var evidences = {}
 
 chrome.storage.local.get(['ont'], function(data) {
   if(data.ont == null){
-    ontology = `@prefix : <http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#> .
+    ontology = `@prefix : <http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#> .
     @prefix owl: <http://www.w3.org/2002/07/owl#> .
     @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
     @prefix xml: <http://www.w3.org/XML/1998/namespace> .
@@ -44804,21 +44805,21 @@ chrome.storage.local.get(['ont'], function(data) {
     #    Object Properties
     #################################################################
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#hasEvidence
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasEvidence
     :hasEvidence rdf:type owl:ObjectProperty ;
                  rdfs:subPropertyOf owl:topObjectProperty ;
                  rdfs:domain :Competency ;
                  rdfs:range :Evidence .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#hasProficiencyLevel
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasProficiencyLevel
     :hasProficiencyLevel rdf:type owl:ObjectProperty ;
                          rdfs:subPropertyOf owl:topObjectProperty ;
                          rdfs:domain :Competency ;
                          rdfs:range :Proficiency_Level .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#isComposedOf
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#isComposedOf
     :isComposedOf rdf:type owl:ObjectProperty ;
                   rdfs:subPropertyOf owl:topObjectProperty ;
                   owl:inverseOf :subsumes ;
@@ -44826,20 +44827,21 @@ chrome.storage.local.get(['ont'], function(data) {
                   rdfs:range :Competency .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#isSimilarTo
-    :isSimilarTo rdf:type owl:ObjectProperty ;
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#isSimilarTo
+    :isSimilarTo rdf:type owl:ObjectProperty ,
+                          owl:SymmetricProperty ;
                  rdfs:domain :Competency ;
                  rdfs:range :Competency .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#requires
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#requires
     :requires rdf:type owl:ObjectProperty ;
               rdfs:subPropertyOf owl:topObjectProperty ;
               rdfs:domain :Competency ;
               rdfs:range :Competency .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#subsumes
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#subsumes
     :subsumes rdf:type owl:ObjectProperty ;
               rdfs:domain :Competency ;
               rdfs:range :Competency .
@@ -44864,13 +44866,13 @@ chrome.storage.local.get(['ont'], function(data) {
               rdfs:domain :Competency .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#name
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#name
     :name rdf:type owl:DatatypeProperty ;
           rdfs:subPropertyOf owl:topDataProperty ;
           rdfs:domain :Competency .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#source
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#source
     :source rdf:type owl:DatatypeProperty ;
             rdfs:subPropertyOf owl:topDataProperty ;
             rdfs:domain :Evidence .
@@ -44880,15 +44882,15 @@ chrome.storage.local.get(['ont'], function(data) {
     #    Classes
     #################################################################
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Competency
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Competency
     :Competency rdf:type owl:Class .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Evidence
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Evidence
     :Evidence rdf:type owl:Class .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Proficiency_Level
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Proficiency_Level
     :Proficiency_Level rdf:type owl:Class ;
                        owl:equivalentClass [ rdf:type owl:Class ;
                                              owl:oneOf ( :Advanced
@@ -44903,9 +44905,31 @@ chrome.storage.local.get(['ont'], function(data) {
     #    Individuals
     #################################################################
     
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Adicionar_propriedades_de_objeto_a_uma_classe
+    :Adicionar_propriedades_de_objeto_a_uma_classe rdf:type owl:NamedIndividual ,
+                                                            :Competency ;
+                                                   :hasProficiencyLevel :Advanced ;
+                                                   :subsumes :Fazer_ontologias_no_protege ;
+                                                   :hasKnowledge "Ontologias" ;
+                                                   :name "Adicionar propriedades de objeto a uma classe" .
+    
+    
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Advanced
+    :Advanced rdf:type owl:NamedIndividual .
+    
+    
     ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Atividade_1
-    :Atividade_1 rdf:type owl:NamedIndividual ;
+    :Atividade_1 rdf:type owl:NamedIndividual ,
+                          :Evidence ;
                  :source "example" .
+    
+    
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Beginner
+    :Beginner rdf:type owl:NamedIndividual .
+    
+    
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Expert
+    :Expert rdf:type owl:NamedIndividual .
     
     
     ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Fazer_ontologias_no_protege
@@ -44913,23 +44937,13 @@ chrome.storage.local.get(['ont'], function(data) {
                                           :Competency ;
                                  :hasEvidence :Atividade_1 ;
                                  :hasProficiencyLevel :Intermediate ;
+                                 :isComposedOf :Adicionar_propriedades_de_objeto_a_uma_classe ;
                                  :hasKnowledge "Ontologias" ;
+                                 :hasSkill "Criar ontologias" ;
                                  :name "Fazer ontologias no protege" .
     
     
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Advanced
-    :Advanced rdf:type owl:NamedIndividual .
-    
-    
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Beginner
-    :Beginner rdf:type owl:NamedIndividual .
-    
-    
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Expert
-    :Expert rdf:type owl:NamedIndividual .
-    
-    
-    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/untitled-ontology-5#Intermediate
+    ###  http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Intermediate
     :Intermediate rdf:type owl:NamedIndividual .
     
     
@@ -44941,104 +44955,6 @@ chrome.storage.local.get(['ont'], function(data) {
   }
   getCompetenciesFromOntology();
 });
-
-// Dimensoes e margens (impressao da arvore)
-const margin = {top: 20, right: 90, bottom: 30, left: 130},
-width  = 660 - margin.left - margin.right,
-height = 500 - margin.top - margin.bottom;
-const treemap = d3.tree().size([height, width]);
-
-// Adiciona SVG no documento (impressao da arvore)
-const svg = d3.select("body").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-
-document.getElementById('add').addEventListener('click', () => {
-  var no = document.getElementById('no').value;
-  var noPai = document.getElementById('no-pai').value
-
-  if (noPai != ''){
-    var pai = competencias.first(function (node) {
-    return node.model.name === noPai;});
-    pai.addChild(tree.parse({name: no, level: 'black'}))
-  }
-  else{
-    competencias.addChild(tree.parse({name: no, level: 'black'}))
-  }
-
-  printArvore(competencias);
-});
-
-document.getElementById('del').addEventListener('click', () => {
-  var no = document.getElementById('no').value;
-  no = competencias.first(function (node) {
-    return node.model.name === no;});
-
-  path = no.getPath()
-  pai = path[path.length-2];
-
-  for(var i = 0; i < no.model.children.length; i++){
-    pai.addChild(tree.parse({name: no.model.children[i].name, level: 'black'}));
-  }
-
-  no.drop();
-  
-  printArvore(competencias);
-});
-
-document.getElementById('del+').addEventListener('click', () => {
-  var no = document.getElementById('no').value;
-  no = competencias.first(function (node) {
-    return node.model.name === no;});
-
-  no.drop();
-  
-  printArvore(competencias);
-});
-
-function printArvore(treeData){
-
-  chrome.storage.local.set({ comp: treeData });
-
-  // Update do SVG
-  d3.selectAll("svg > *").remove();  
-  var g = svg.append("g")
-  .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
-
-  //  Adiciona os dados para a hierarquia da arvore
-  let nodes = d3.hierarchy(treeData, d => d.children);
-
-  nodes = treemap(nodes);
-
-  // Links entre os nós
-  const link = g.selectAll(".link")
-  .data( nodes.descendants().slice(1))
-  .enter().append("path")
-  .attr("class", "link")
-  .style("stroke", d => d.data.model.level)
-  .attr("d", d => {
-  return "M" + d.y + "," + d.x
-    + "C" + (d.y + d.parent.y) / 2 + "," + d.x
-    + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
-    + " " + d.parent.y + "," + d.parent.x;
-  });
-
-  // Adiciona cada nó como um grupo
-  const node = g.selectAll(".node")
-  .data(nodes.descendants())
-  .enter().append("g")
-  .attr("class", d => "node" + (d.children ? " node--internal" : " node--leaf"))
-  .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
-
-  // Texto do nó
-  node.append("text")
-  .attr("dy", ".35em")
-  .attr("x", d => d.children ? 5 * -1 : 5)
-  .attr("y", d => d.children && d.depth !== 0 ? - 8 : 0)
-  .style("text-anchor", d => d.children ? "end" : "start")
-  .text(d => d.data.model.name);
-}
 
 function getCompetenciesFromOntology(){
 
@@ -45052,7 +44968,8 @@ function getCompetenciesFromOntology(){
         quadType(quad);
       }
       else{
-        console.log("# That's all, folks!", prefixes);
+        console.log(competencias);
+        console.log(evidences);
       }
     });
 
@@ -45061,13 +44978,82 @@ function getCompetenciesFromOntology(){
 function quadType(quad){
   // is competency
   if(quad._object.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Competency' && quad._predicate.id == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'){
-    
+      // verifica se ja esta na arvore
+      if(competencias.first(function (node) {
+      return node.model.id === quad._subject.id;}) == null){
+        competencias.addChild(tree.parse({id: quad._subject.id}));
+      }
   }
   // has evidence
   else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasEvidence'){
-    
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.evidence =  quad._object.id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '');
   }
-}
+  // name
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#name'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.name =  quad._object.id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '').replace(/\"/gi, '');
+  }
+  // subsumes
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#subsumes'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    var parent = competencias.first(function (node) {
+      return node.model.id === quad.object.id;});
+    if(parent == null){
+      competencias.addChild(tree.parse({id: quad._object.id}));
+      parent = competencias.first(function (node) {
+        return node.model.id === quad.object.id;});
+    }
+    comp = comp.drop();
+    parent.addChild(comp);
+  }
+  // hasSkill
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasSkill'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.skill =  quad._object.id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '').replace(/\"/gi, '');
+  }
+  // hasKnowledge
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasKnowledge'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.knowledge =  quad._object.id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '').replace(/\"/gi, '');
+  }
+  // hasProficiencyLevel
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#hasProficiencyLevel'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.proficiencyLevel =  quad._object.id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '');
+  }
+  // requires
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#requires'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.requires =  quad._object.id;
+  }
+  // isSimilarTo
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#isSimilarTo'){
+    var comp = competencias.first(function (node) {
+      return node.model.id === quad._subject.id;});
+    comp.model.similar =  quad._object.id;
+  }
+
+  // Evidencias:
+
+  // is evidence
+  else if(quad._object.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#Evidence' && quad._predicate.id == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'){
+    var id = quad._subject.id;
+    evidences[id] = {name : id.replace('http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#', '').replace(/_/gi, '')};
+  }
+  // source
+  else if(quad._predicate.id == 'http://www.semanticweb.org/gabriel/ontologies/2022/4/competencies#source'){
+    var id = quad._subject.id;
+    evidences[id].source = quad._object.id;
+  }
+}    
 })();
 
 /******/ })()
